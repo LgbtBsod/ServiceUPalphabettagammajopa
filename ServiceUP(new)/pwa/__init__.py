52a3b9a5338a9d9eb4561565cc5abd56731824b8
postaldead-byte <postaldead@gmail.com>
2026-08-14 12:03:53 +0400

    Delete ServiceUP(new) directory

--- ServiceUP(new)/pwa/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""PWA-пакет: мобильная версия приложения (Flask-сервер + фронтенд)."""
-
-from pwa.server import PWAServerManager, get_local_ip
-
-__all__ = ['PWAServerManager', 'get_local_ip']
